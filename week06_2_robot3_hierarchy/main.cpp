d9f2079bf35c7110836ec55017a19e0e711076fe
vickylo0919 <jingyilo0919@gmail.com>
2023-03-22 11:55:01 +0800

    week06

---
+++ week06_2_robot3_hierarchy/main.cpp
@@ -0,0 +1,38 @@
+#include <GL/glut.h>
+float angle = 0;
+void display()
+{
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+        ///glutSolidSphere(0.02 , 30 , 30); ///圓在正中心
+
+    glutSolidTeapot(0.3); ///身體
+    glPushMatrix();
+            ///glTranslatef( 0.5 , 0.5 , 0 ); 先註解掉
+        glTranslatef( 0.49 , 0.13 , 0 );
+        glRotatef(angle,0,0,1);
+        glTranslatef(0.46,-0.05,0);
+        glutSolidTeapot(0.3);
+
+        glPushMatrix(); ///右手肘
+            glTranslatef( 0.49 , 0.13 , 0 );
+            glRotatef(angle,0,0,1);
+            glTranslatef(0.46,-0.05,0);
+            glutSolidTeapot(0.3);
+        glPopMatrix();
+    glPopMatrix();
+
+    glutSwapBuffers();
+    angle++;
+}
+int main(int argc, char* argv[] )
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week05");
+
+    glutDisplayFunc(display);
+    glutIdleFunc(display);
+    glutMainLoop();
+}
+
