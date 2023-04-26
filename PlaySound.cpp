9546ef09edc75fb1c1d58dcbd817159faddd2ea0
vickylo0919 <jingyilo0919@gmail.com>
2023-04-26 12:03:09 +0800

    week11

---
+++ PlaySound.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+int main()
+{
+    PlaySound("C:\\Users\\Administrator\\Desktop\\do_re_mi\\do.wav",NULL,SND_SYNC);
+    PlaySound("C:\\Users\\Administrator\\Desktop\\do_re_mi\\do.wav",NULL,SND_SYNC);
+    PlaySound("C:\\Users\\Administrator\\Desktop\\do_re_mi\\do.wav",NULL,SND_SYNC);
+}
